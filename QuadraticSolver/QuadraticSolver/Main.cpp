647b56ebea9a48a6d6f4c28127bef011dbde084f
rsoderberg <r_soderberg@yahoo.com>
2018-01-06 14:15:09 -0800

    Update to QuadraticSolver

--- QuadraticSolver/QuadraticSolver/Main.cpp
+++ QuadraticSolver/QuadraticSolver/Main.cpp
@@ -8,12 +8,9 @@
 //          a * x^2 + b * x + c = 0
 // To solve these, one uses the quadratic formula:
 //          x = (-b +/- sqrt(b^2 - 4ac)) / 2a
-// There is a problem, though: if b^2 - 4ac is less than zero, then it will fail.
-// - Write a program that can calculate x for a quadratic equation.
-// - Create a function that prints out the roots of a quadratic equation, given a, b, c.
-// - When the program detects an equation with no real roots, have it print out a message.
+// This program will calculate the roots for a quadratic equation, given a, b, and c.
 // Roots are exactly the x-intercepts of the quadratic function f(x) = a * x^2 + b * x + c = 0,
-// that is the intersection between the graph of the quadratic function with the x-axis. 
+// that is the intersection between the graph of the quadratic function with the x-axis.  
 
 #include "Main.h"
 
