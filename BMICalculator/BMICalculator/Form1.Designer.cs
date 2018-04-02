0eaab3123e1d2a368b8a59063629b41a09174c81
rsoderberg <r_soderberg@yahoo.com>
2018-04-02 16:06:47 -0700

    Added BMI Calculator

---
+++ BMICalculator/BMICalculator/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace BMICalculator
+{
+    partial class bmiCalcForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.bmiLabel = new System.Windows.Forms.Label();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.weightTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Height (inches)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Weight (pounds)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Your BMI:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(112, 72);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(96, 36);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate!";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // bmiLabel
+            // 
+            this.bmiLabel.AutoSize = true;
+            this.bmiLabel.Location = new System.Drawing.Point(180, 135);
+            this.bmiLabel.Name = "bmiLabel";
+            this.bmiLabel.Size = new System.Drawing.Size(0, 17);
+            this.bmiLabel.TabIndex = 4;
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Location = new System.Drawing.Point(60, 29);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(57, 22);
+            this.heightTextBox.TabIndex = 5;
+            // 
+            // weightTextBox
+            // 
+            this.weightTextBox.Location = new System.Drawing.Point(203, 29);
+            this.weightTextBox.Name = "weightTextBox";
+            this.weightTextBox.Size = new System.Drawing.Size(57, 22);
+            this.weightTextBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "At this BMI, you are considered:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(223, 170);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            this.statusLabel.TabIndex = 8;
+            // 
+            // bmiCalcForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 208);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.weightTextBox);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.bmiLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(341, 255);
+            this.MinimumSize = new System.Drawing.Size(341, 255);
+            this.Name = "bmiCalcForm";
+            this.Text = "BMI Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label bmiLabel;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.TextBox weightTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ BMICalculator/BMICalculator/Form1.cs
@@ -0,0 +1,67 @@
+﻿// Project: Body Mass Index (BMI) Calculator
+// Author: Rachel Soderberg
+// Date Created: April 2, 2018
+// Latest Edit: April 2, 2018
+
+// Description: Convert user's height and weight to meters and kilograms, respectively, and
+//              calculate their BMI with their current status (under, normal, or overweight).
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMICalculator
+{
+    public partial class bmiCalcForm : Form
+    {
+        public bmiCalcForm()
+        {
+            InitializeComponent();
+        }
+
+        public void calculateBmi()
+        {
+            int height = 0, weight = 0;
+            double bmi = 0;
+
+            height = Convert.ToInt32(heightTextBox.Text);
+            weight = Convert.ToInt32(weightTextBox.Text);
+
+            // Convert inputs to metric
+            double heightMeters = height * 0.0254;
+            double heightSquared = heightMeters * heightMeters;
+            double weightKilograms = weight * 0.453592;
+
+            // Metric BMI formula: Weight (kg) / (Height (m))².
+            bmi = weightKilograms / heightSquared;
+
+            int roundBMI = Convert.ToInt32(bmi);
+            bmiLabel.Text = Convert.ToString(roundBMI);
+
+            if (bmi < 18.5)
+                statusLabel.Text = "Underweight";
+            else if (bmi >= 18.5 && bmi <= 24.9)
+                statusLabel.Text = "Normal";
+            else if (bmi > 25)
+                statusLabel.Text = "Overweight";
+            else
+                statusLabel.Text = "Error";
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            if (heightTextBox.Text == string.Empty || weightTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("Error, must input height and weight!");
+            }
+            else
+                calculateBmi();
+        }
+    }
+}
