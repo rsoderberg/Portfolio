bcd79356dbd12af946500985b1ab9362a2ee715a
Rachel Soderberg <rsoderberg@boxxtech.com>
2019-08-22 12:15:55 -0500

    Initial Commit - Caesar Cipher Project
    Basic input/output testing setup, read in a key and message and print them back to the console.

---
+++ C# Projects/Caesar Cipher/Caesar Cipher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        // Dev.to Daily Challenge #42 - Caesar Cipher
+        // https://dev.to/thepracticaldev/daily-challenge-42-caesar-cipher-43k8
+
+        // - Accept a shift key and a string and return the encrypted text. Leave any non-letter characters as-is.
+        // - To encrypt, substitute the letters in the message with letters some fixed number of positions (key)
+        // down the alphabet.
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the encryption shift key (1-9): ");
+            var key = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the sentence to encrypt: ");
+            var message = Console.ReadLine();
+
+            Console.WriteLine(key);
+            Console.WriteLine(message);
+
+            Console.Read();
+        }
+    }
+}
