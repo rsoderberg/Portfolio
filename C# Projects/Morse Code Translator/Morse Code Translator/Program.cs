31dce7c5397c048321dd2d0c50006fcebd6e1fa2
rsoderberg <r_soderberg@yahoo.com>
2019-09-02 06:22:23 -0500

    Added new project (Morse Code Translator)

---
+++ C# Projects/Morse Code Translator/Morse Code Translator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morse_Code_Translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
