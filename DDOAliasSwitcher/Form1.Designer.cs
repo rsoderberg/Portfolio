811a5ad3796218faedb84669c1a0756fd066fe50
rsoderberg <r_soderberg@yahoo.com>
2022-10-11 00:25:20 -0500

    Create project, flesh out basic form concept

---
+++ DDOAliasSwitcher/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace DDOAliasSwitcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.monButton = new System.Windows.Forms.Button();
+            this.tuesButton = new System.Windows.Forms.Button();
+            this.wedButton = new System.Windows.Forms.Button();
+            this.thursButton = new System.Windows.Forms.Button();
+            this.friButton = new System.Windows.Forms.Button();
+            this.satAMButton = new System.Windows.Forms.Button();
+            this.sunButton = new System.Windows.Forms.Button();
+            this.satPMButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Location:";
+            // 
+            // monButton
+            // 
+            this.monButton.Location = new System.Drawing.Point(85, 112);
+            this.monButton.Name = "monButton";
+            this.monButton.Size = new System.Drawing.Size(75, 23);
+            this.monButton.TabIndex = 2;
+            this.monButton.Text = "Monday";
+            this.monButton.UseVisualStyleBackColor = true;
+            // 
+            // tuesButton
+            // 
+            this.tuesButton.Location = new System.Drawing.Point(201, 112);
+            this.tuesButton.Name = "tuesButton";
+            this.tuesButton.Size = new System.Drawing.Size(75, 23);
+            this.tuesButton.TabIndex = 3;
+            this.tuesButton.Text = "Tuesday";
+            this.tuesButton.UseVisualStyleBackColor = true;
+            // 
+            // wedButton
+            // 
+            this.wedButton.Location = new System.Drawing.Point(317, 112);
+            this.wedButton.Name = "wedButton";
+            this.wedButton.Size = new System.Drawing.Size(79, 23);
+            this.wedButton.TabIndex = 4;
+            this.wedButton.Text = "Wednesday";
+            this.wedButton.UseVisualStyleBackColor = true;
+            // 
+            // thursButton
+            // 
+            this.thursButton.Location = new System.Drawing.Point(32, 170);
+            this.thursButton.Name = "thursButton";
+            this.thursButton.Size = new System.Drawing.Size(75, 23);
+            this.thursButton.TabIndex = 5;
+            this.thursButton.Text = "Thursday";
+            this.thursButton.UseVisualStyleBackColor = true;
+            // 
+            // friButton
+            // 
+            this.friButton.Location = new System.Drawing.Point(146, 170);
+            this.friButton.Name = "friButton";
+            this.friButton.Size = new System.Drawing.Size(75, 23);
+            this.friButton.TabIndex = 6;
+            this.friButton.Text = "Friday";
+            this.friButton.UseVisualStyleBackColor = true;
+            // 
+            // satAMButton
+            // 
+            this.satAMButton.Location = new System.Drawing.Point(260, 159);
+            this.satAMButton.Name = "satAMButton";
+            this.satAMButton.Size = new System.Drawing.Size(85, 23);
+            this.satAMButton.TabIndex = 7;
+            this.satAMButton.Text = "SaturdayAM";
+            this.satAMButton.UseVisualStyleBackColor = true;
+            // 
+            // sunButton
+            // 
+            this.sunButton.Location = new System.Drawing.Point(384, 170);
+            this.sunButton.Name = "sunButton";
+            this.sunButton.Size = new System.Drawing.Size(75, 23);
+            this.sunButton.TabIndex = 8;
+            this.sunButton.Text = "Sunday";
+            this.sunButton.UseVisualStyleBackColor = true;
+            // 
+            // satPMButton
+            // 
+            this.satPMButton.Location = new System.Drawing.Point(260, 188);
+            this.satPMButton.Name = "satPMButton";
+            this.satPMButton.Size = new System.Drawing.Size(85, 23);
+            this.satPMButton.TabIndex = 9;
+            this.satPMButton.Text = "SaturdayPM";
+            this.satPMButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Other File Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 238);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 23);
+            this.textBox2.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 312);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.satPMButton);
+            this.Controls.Add(this.sunButton);
+            this.Controls.Add(this.satAMButton);
+            this.Controls.Add(this.friButton);
+            this.Controls.Add(this.thursButton);
+            this.Controls.Add(this.wedButton);
+            this.Controls.Add(this.tuesButton);
+            this.Controls.Add(this.monButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(517, 351);
+            this.MinimumSize = new System.Drawing.Size(517, 351);
+            this.Name = "Form1";
+            this.Text = "DDO Layout File Switcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button monButton;
+        private Button tuesButton;
+        private Button wedButton;
+        private Button thursButton;
+        private Button friButton;
+        private Button satAMButton;
+        private Button sunButton;
+        private Button satPMButton;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
---
+++ DDOAliasSwitcher/Form1.cs
@@ -0,0 +1,14 @@
+namespace DDOAliasSwitcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // C:\Users\r_sod\Documents\Dungeons and Dragons Online\ui\layouts\
+
+            // How to allow the user to search for their file(s) automatically?
+
+            InitializeComponent();
+        }
+    }
+}
