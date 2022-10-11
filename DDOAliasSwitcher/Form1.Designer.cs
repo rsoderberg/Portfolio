8086b0b391adfc1591b2107f2d6a21659f5d9604
rsoderberg <r_soderberg@yahoo.com>
2022-10-11 15:08:21 -0500

    Added a second Sat Morning button

--- DDOAliasSwitcher/Form1.Designer.cs
+++ DDOAliasSwitcher/Form1.Designer.cs
@@ -35,25 +35,28 @@
             this.wedButton = new System.Windows.Forms.Button();
             this.thursButton = new System.Windows.Forms.Button();
             this.friButton = new System.Windows.Forms.Button();
-            this.satAMButton = new System.Windows.Forms.Button();
+            this.satAM1Button = new System.Windows.Forms.Button();
             this.sunButton = new System.Windows.Forms.Button();
             this.satPMButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.locButton = new System.Windows.Forms.Button();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.satAM2Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(113, 54);
+            this.textBox1.Location = new System.Drawing.Point(115, 33);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(334, 23);
+            this.textBox1.Size = new System.Drawing.Size(307, 23);
             this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 57);
+            this.label1.Location = new System.Drawing.Point(32, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 15);
             this.label1.TabIndex = 1;
@@ -61,7 +64,7 @@
             // 
             // monButton
             // 
-            this.monButton.Location = new System.Drawing.Point(85, 112);
+            this.monButton.Location = new System.Drawing.Point(87, 88);
             this.monButton.Name = "monButton";
             this.monButton.Size = new System.Drawing.Size(75, 23);
             this.monButton.TabIndex = 2;
@@ -70,7 +73,7 @@
             // 
             // tuesButton
             // 
-            this.tuesButton.Location = new System.Drawing.Point(201, 112);
+            this.tuesButton.Location = new System.Drawing.Point(203, 88);
             this.tuesButton.Name = "tuesButton";
             this.tuesButton.Size = new System.Drawing.Size(75, 23);
             this.tuesButton.TabIndex = 3;
@@ -79,7 +82,7 @@
             // 
             // wedButton
             // 
-            this.wedButton.Location = new System.Drawing.Point(317, 112);
+            this.wedButton.Location = new System.Drawing.Point(319, 88);
             this.wedButton.Name = "wedButton";
             this.wedButton.Size = new System.Drawing.Size(79, 23);
             this.wedButton.TabIndex = 4;
@@ -88,7 +91,7 @@
             // 
             // thursButton
             // 
-            this.thursButton.Location = new System.Drawing.Point(32, 170);
+            this.thursButton.Location = new System.Drawing.Point(34, 146);
             this.thursButton.Name = "thursButton";
             this.thursButton.Size = new System.Drawing.Size(75, 23);
             this.thursButton.TabIndex = 5;
@@ -97,25 +100,25 @@
             // 
             // friButton
             // 
-            this.friButton.Location = new System.Drawing.Point(146, 170);
+            this.friButton.Location = new System.Drawing.Point(148, 146);
             this.friButton.Name = "friButton";
             this.friButton.Size = new System.Drawing.Size(75, 23);
             this.friButton.TabIndex = 6;
             this.friButton.Text = "Friday";
             this.friButton.UseVisualStyleBackColor = true;
             // 
-            // satAMButton
+            // satAM1Button
             // 
-            this.satAMButton.Location = new System.Drawing.Point(260, 159);
-            this.satAMButton.Name = "satAMButton";
-            this.satAMButton.Size = new System.Drawing.Size(85, 23);
-            this.satAMButton.TabIndex = 7;
-            this.satAMButton.Text = "SaturdayAM";
-            this.satAMButton.UseVisualStyleBackColor = true;
+            this.satAM1Button.Location = new System.Drawing.Point(243, 135);
+            this.satAM1Button.Name = "satAM1Button";
+            this.satAM1Button.Size = new System.Drawing.Size(57, 23);
+            this.satAM1Button.TabIndex = 7;
+            this.satAM1Button.Text = "SatAM1";
+            this.satAM1Button.UseVisualStyleBackColor = true;
             // 
             // sunButton
             // 
-            this.sunButton.Location = new System.Drawing.Point(384, 170);
+            this.sunButton.Location = new System.Drawing.Point(386, 146);
             this.sunButton.Name = "sunButton";
             this.sunButton.Size = new System.Drawing.Size(75, 23);
             this.sunButton.TabIndex = 8;
@@ -124,7 +127,7 @@
             // 
             // satPMButton
             // 
-            this.satPMButton.Location = new System.Drawing.Point(260, 188);
+            this.satPMButton.Location = new System.Drawing.Point(262, 164);
             this.satPMButton.Name = "satPMButton";
             this.satPMButton.Size = new System.Drawing.Size(85, 23);
             this.satPMButton.TabIndex = 9;
@@ -134,7 +137,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(61, 241);
+            this.label2.Location = new System.Drawing.Point(63, 217);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 15);
             this.label2.TabIndex = 10;
@@ -142,31 +145,61 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(163, 238);
+            this.textBox2.Location = new System.Drawing.Point(165, 214);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(201, 23);
             this.textBox2.TabIndex = 11;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(370, 238);
+            this.button1.Location = new System.Drawing.Point(372, 214);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(50, 23);
             this.button1.TabIndex = 12;
             this.button1.Text = "Load";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // locButton
+            // 
+            this.locButton.Location = new System.Drawing.Point(428, 33);
+            this.locButton.Name = "locButton";
+            this.locButton.Size = new System.Drawing.Size(33, 23);
+            this.locButton.TabIndex = 13;
+            this.locButton.Text = "...";
+            this.locButton.UseVisualStyleBackColor = true;
+            // 
+            // helpButton
+            // 
+            this.helpButton.Location = new System.Drawing.Point(414, 277);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(75, 23);
+            this.helpButton.TabIndex = 14;
+            this.helpButton.Text = "Help";
+            this.helpButton.UseVisualStyleBackColor = true;
+            // 
+            // satAM2Button
+            // 
+            this.satAM2Button.Location = new System.Drawing.Point(309, 135);
+            this.satAM2Button.Name = "satAM2Button";
+            this.satAM2Button.Size = new System.Drawing.Size(57, 23);
+            this.satAM2Button.TabIndex = 15;
+            this.satAM2Button.Text = "SatAM2";
+            this.satAM2Button.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(501, 312);
+            this.Controls.Add(this.satAM2Button);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.locButton);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.satPMButton);
             this.Controls.Add(this.sunButton);
-            this.Controls.Add(this.satAMButton);
+            this.Controls.Add(this.satAM1Button);
             this.Controls.Add(this.friButton);
             this.Controls.Add(this.thursButton);
             this.Controls.Add(this.wedButton);
@@ -192,11 +225,14 @@
         private Button wedButton;
         private Button thursButton;
         private Button friButton;
-        private Button satAMButton;
+        private Button satAM1Button;
         private Button sunButton;
         private Button satPMButton;
         private Label label2;
         private TextBox textBox2;
         private Button button1;
+        private Button locButton;
+        private Button helpButton;
+        private Button satAM2Button;
     }
 }
