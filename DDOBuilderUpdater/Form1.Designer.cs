930825dee75a0aee0e63d4a1019bf77a0bfd6a2b
rsoderberg <r_soderberg@yahoo.com>
2023-02-27 18:49:29 -0600

    Add DDOBuilderUpdater project

---
+++ DDOBuilderUpdater/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace DDOBuilderUpdater
+{
+    partial class UpdaterForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Location = new System.Drawing.Point(12, 12);
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.Size = new System.Drawing.Size(330, 465);
+            this.mainTextBox.TabIndex = 0;
+            this.mainTextBox.Text = "";
+            // 
+            // UpdaterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 489);
+            this.Controls.Add(this.mainTextBox);
+            this.Name = "UpdaterForm";
+            this.Text = "Update Maetrim DDO Builder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox mainTextBox;
+    }
+}
---
+++ DDOBuilderUpdater/Form1.cs
@@ -0,0 +1,17 @@
+namespace DDOBuilderUpdater
+{
+    public partial class UpdaterForm : Form
+    {
+        public UpdaterForm()
+        {
+            InitializeComponent();
+
+            DoStuff();
+        }
+
+        private void DoStuff()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
